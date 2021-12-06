b1ece9c7b61095b8185193a098eabb443fd6df32
BryantCR <bryancvargas20@gmail.com>
2021-12-06 11:59:51 -0600

    ok

---
+++ server/controllers/ApiController.js
@@ -0,0 +1,138 @@
+const {BakerModel} = require('./../models/ApiModel');
+
+const BakerController = {
+
+    allCakes: function(req, response){
+        BakerModel
+        .getAllCakes()
+        .then( data => {
+            let task = data.map(tasks => {
+                console.log( tasks );
+                return {
+                    _id: tasks._id,
+                    firstname: tasks.firstname,
+                    lastname : tasks.lastname,
+                    image : tasks.image,
+                    created_at : tasks.created_at,
+                    id2: tasks.baker_id
+                }
+            })
+        console.log( task );
+        response.status( 200 ).json( {message: "Success!", task: task} );
+        })
+        .catch( err => {
+            console.log( "Something went wrong!" );
+            console.log( err );
+            response.json( err );
+        })
+    },
+
+    addCake: function(request, response){
+
+        let firstname = request.body.firstname;
+        let lastname = request.body.lastname;
+        let image = request.body.image;
+        let created_at = new Date();
+
+        if(firstname){
+            newCake = {
+                firstname,
+                lastname,
+                image,
+                created_at,
+            }
+            console.log("New Baker info: ", newCake);
+
+            TaskModel
+                .createTask( newCake )
+                .then( result => {
+                    response.status( 201 ).json( {message: "Success!", added: true, task: result } );
+                });
+        }
+        else{
+            response.statusMessage = "You are missing a field to create a new baker ('Firstname')";
+            response.status( 406 ).end();
+        }  
+    },
+
+    findById : function ( request, response ) {
+        let id2 = request.params.id2;
+        console.log("HERE", id2);
+
+        BakerModel
+            .getTaskById(id2)
+            .then( titles => {
+                let cake = titles
+                console.log("HERE", cake);
+                response.status( 200 ).json( {message: "Success!", cake : cake} );
+            })
+    },
+
+    removeTask : function(request, response){
+        let title = request.params.title;
+        console.log("HERE222 :", title);
+
+        BakerModel
+            .getTaskByName( title )
+            .then( result => {
+                if( result === null ){
+                    console.log( "Something went wrong!" );
+                }
+                else{
+                    TaskModel
+                        .delete( title )
+                        .then( result => {
+                            response.status( 204 ).end();
+                        });
+                }
+            })
+    },
+
+    update : function(request, response) {
+        let title = request.params.title;
+        console.log("Success L1 :", title);
+
+        BakerModel
+            .getTaskByName(title)
+            .then(result => {
+                if( result === null ){
+                    console.log( "Something went wrong!" );
+                    response.json({message: "Error!", error: err});
+                }
+                else {
+                    let task = result;
+                    console.log("HERE", task);
+                    response.status( 200 ).json( {message: "Success!", cake : cake} );
+                    if(task.title === request.body.title){
+                        response.json({message: "Error!"});
+                    }
+                    if(request.body.title){
+                        task.title = request.body.title;
+                    }
+                    if(request.body.description){
+                        task.description = request.body.description;
+                    }
+                    if(request.params.completed){
+                        task.completed = request.body.completed;
+                    }
+                    task.save(function(err){
+                        if(err){
+                            console.log("AHHHHHHHHHHHHHH EROR 1");
+                            response.json({message: "Error!", error: err});
+                        }
+                        else{
+                            console.log("AHHHHHHHHHHHHHH EROR 2");
+                            console.log("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW ", task);
+                            response.status(200).json({message: "Success!", task: task})
+                        }
+                    })
+                }
+            })
+            
+    }
+    
+}
+
+module.exports = {BakerController};
+
+//----------------------------------------------------dadaaad
---
+++ server/models/ApiModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const {CakeSchema} = require( './CakeModel' );
+const AutoIncrement = require('mongoose-sequence')(mongoose); // Auto-Increment
+
+const BakerSchema = new mongoose.Schema({
+    firstname: {
+    type: String,
+    trim: true,
+    required: [true, 'Baker name is required'],
+    minlength: [3, 'Baker Name length must be greater than 3'],
+    unique: false,
+    default: ""
+    },
+    lastname: {
+    type: String,
+    minlength: [3, 'Baker LastName length must be greater than 3'],
+    trim: true,
+    default: ""
+    },
+    image: {
+    type: String, 
+    default: ""
+    },
+    ratings: [CakeSchema],
+    created_at : {
+        type : Date,
+    },
+});
+BakerSchema.plugin(AutoIncrement, {inc_field: 'baker_id'});
+const Baker = mongoose.model("bakers", BakerSchema);
+
+const BakerModel = {
+    addCake : function( newCake ){
+        return Baker.create( newCake );
+    },
+    getAllCakes : function(){
+        return Baker.find();
+    },
+    getCakeByName : function( title ){
+        return Baker.findOne({ title });
+    },
+    delete : function( title ){
+        return Baker.deleteOne({ title });
+    },
+    updateTask: function(id, data){
+        return Baker.findOneAndUpdate({id:id},{$set:data})
+    }
+};
+
+module.exports = {BakerModel};
